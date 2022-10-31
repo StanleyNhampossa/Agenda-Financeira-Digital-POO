0c62840c35b8d45addf94c92c9c7d07959c8b6b0
Paulo Massingue <paulomassingue31@gmail.com>
2022-10-31 12:23:20 +0100

    CRICAO DA CONNEXACAO A BANCO DE DADOS

---
+++ src/Banco/Conectar.java
@@ -0,0 +1,29 @@
+
+package Banco;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+// conexao a banco de dados 
+public class Conectar {
+    
+    private static final String usuario="root";
+    private static final String senha="";
+    private static final String url="jdbc:mysql://localhost/teste2poo";
+    
+    public static Connection getConectar(){
+    Connection conexao=null;
+    try{
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    conexao=DriverManager.getConnection(url,usuario,senha);
+        System.out.println("Conectado");
+    }catch(Exception ex){
+    JOptionPane.showMessageDialog(null,"ERRO NA CONEXAO"+ex.getMessage());
+    }
+    return conexao;
+   
+    } 
+    
+    
+}
